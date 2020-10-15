40aa1042ef15ca305663c81c301a43c6c12d4dd9
kombiKarma <peaswildwind@gmail.com>
2020-10-15 10:25:03 +0300

    Main.java added

---
+++ src/Terms.java
@@ -0,0 +1,12 @@
+public class Terms {
+    String id;
+    String name;
+    int callLimit;
+    String addition;
+    int internetLimit;
+    String additionIL;
+    String pictureUrl;
+    short cost;
+    String text;
+    String button;
+}
