bc2b26a8978abded6a516ebac76e1ee71e7cc61d
kombiKarma <peaswildwind@gmail.com>
2020-10-20 12:01:43 +0300

    option 1

--- src/ChoiceTariff.java
+++ src/ChoiceTariff.java
@@ -1,26 +1,8 @@
 public class ChoiceTariff {
     public static void main(String[] args) {
-        String id;
-        String name = "Везде онлайн";
-        int callLimit = 500;
-        String addition = "+ безлимит на Tele2 России";
-        int internetLimit = 40;
-        String additionIL = "+безлимитные";
-        String pictureUrl;
-        Short cost = 500;
-        String text = "Рублей/месяц";
-        String button = "Купить SIM";
+
         Terms tariff = new Terms();
-        System.out.println(tariff.id);
-        System.out.println(tariff.name);
-        System.out.println(tariff.callLimit);
-        System.out.println(tariff.addition);
-        System.out.println(tariff.internetLimit);
-        System.out.println(tariff.additionIL);
-        System.out.println(tariff.pictureUrl);
-        System.out.println(tariff.cost);
-        System.out.println(tariff.text);
-        System.out.println(tariff.button);
+
         tariff.id = null;
         tariff.name = "Везде онлайн";
         tariff.callLimit = 500;
@@ -31,5 +13,16 @@ public class ChoiceTariff {
         tariff.cost = 500;
         tariff.text = "Рублей/месяц";
         tariff.button = "Купить SIM";
+
+        System.out.println((String) null);
+        System.out.println(tariff.name);
+        System.out.println(tariff.callLimit);
+        System.out.println(tariff.addition);
+        System.out.println(tariff.internetLimit);
+        System.out.println(tariff.additionIL);
+        System.out.println((String) null);
+        System.out.println(tariff.cost);
+        System.out.println(tariff.text);
+        System.out.println(tariff.button);
     }
 }
